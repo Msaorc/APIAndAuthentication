1913118022beead00e5c7b1d8b1bf91ae49d1281
Marcos Augusto <msaorc@hotmail.com>
2023-01-27 11:25:44 -0300

    API that uses controllers to organize its endpoints, with authentication using JWT

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using APIFull.Models;
 using APIFull.Repositories;
 using APIFull.Services;
+using Microsoft.IdentityModel.Tokens;
 
 namespace APIFull.Controllers
 {
@@ -11,7 +12,7 @@ namespace APIFull.Controllers
     {
         [HttpPost]
         [Route("login")]
-        public async Task<ActionResult<dynamic>> AuthenticateAsync(User bodyUser)
+        public async Task<ActionResult<dynamic>> AuthenticateAsync([FromBody] User bodyUser)
         {
             var user = UserRepository.Get(bodyUser.UserName, bodyUser.PassWord);
 
@@ -20,15 +21,41 @@ namespace APIFull.Controllers
                 return NotFound(new {message = "Invalid username or password"});
             }
 
-            var token = TokenService.GenerateToken(user);
+            var token = TokenService.GenerateTokenByUser(user);
+            var refreshToken = TokenService.GenerateRefreshToken();
+            TokenService.SaveRefreshToken(user.UserName, refreshToken);
 
             user.PassWord = "";
 
             return new {
                 message = "Token is generate with success",
                 user = user,
-                token = token
+                token = token,
+                refresToken = refreshToken
             };
+        }
+
+        [HttpPost]
+        [Route("refreshToken")]
+        public IActionResult RefreshToken([FromBody] TokenObject tokenObject)
+        {
+            var principal = TokenService.GetPrincipalFromExpiredToken(tokenObject.Token);
+            var username = principal.Identity.Name;
+            var savedRefreshToken = TokenService.GetRefreshToken(username);
+            if (savedRefreshToken != tokenObject.RefreshToken)
+            {
+                throw new SecurityTokenException("Invalid token");
+            }
+
+            var newJWtToken = TokenService.GenerateToken(principal.Claims);
+            var newRefreshToken = TokenService.GenerateRefreshToken();
+            TokenService.DeleteFrefreshToken(username, tokenObject.RefreshToken);
+            TokenService.SaveRefreshToken(username, newRefreshToken);
+
+            return new ObjectResult(new{
+                token = newJWtToken,
+                refreshToken = newRefreshToken
+            });
         }
     }
 }
---
+++ Models/TokenObject.cs
@@ -0,0 +1,8 @@
+namespace APIFull.Models
+{
+    public class TokenObject
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
